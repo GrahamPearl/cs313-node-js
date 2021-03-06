fcf9637aceec147ca9fd113ff24bcc02fcbd586b
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2021-03-06 18:23:39 +0200

    Update

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const PORT = process.env.PORT || 5000
 
 var bodyParser = require("body-parser");
 
+function calculateRate(mail_type, weight, zone) {
+  return 100;
+}
+
 express()
   .use(bodyParser.urlencoded({
     extended: false
   }))
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'views/models')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/postmail'))
   .post('/report', (req, res) => {
     var params = {
-      weight: req.body.weight,
-      type: req.body.type,
-      zone: req.body.zone
+      weight: req.body.weight
+      ,type: req.body.type      
+      ,amount: calculateRate(req.body.type, req.body.weight, req.body.zone)
     }
     res.render("pages/report", params);
   })
