84886bff3ab149d16592e28bb063bb99097cf6e0
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2021-03-09 04:49:43 +0200

    Update

--- index.js
+++ index.js
@@ -4,66 +4,6 @@ const PORT = process.env.PORT || 5000
 
 var bodyParser = require("body-parser");
 
-function closest(arr, closestTo) {
-  var closest = Math.max.apply(null, arr);
-
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] >= closestTo && arr[i] < closest) closest = arr[i];
-  }
-
-  return closest; // return the value
-};
-
-function calculateRate(mail_type, weight, zone) {
-  let cost = 200;
-  let weights = [];
-  let amount = [];
-  let index = 0;
-  switch (mail_type) {
-    case "1":
-      weights = [1, 2, 3, 3.5];
-      index = [closest(weights, weight)];
-
-      amount = [0.55, 0.75, 0.95, 1.15];      
-      cost = amount[index - 1];
-      break;
-
-    case "2":
-      weights = [1, 2, 3, 3.5];
-      index = [closest(weights, weight)];
-
-      amount = [0.51, 0.71, 0.91, 1.11];      
-      cost = amount[index - 1];
-      break;
-
-    case "3":
-      weights = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-      index = [closest(weights, weight)];
-
-      amount = [1.00, 1.20, 1.40, 1.60, 1.80, 2.00, 2.20, 2.40, 2.60, 2.80, 3.00, 3.20, 3.40];      
-      cost = amount[index - 1];
-      break;
-
-    case "4":
-      weights = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-      index = [closest(weights, weight)];
-          amount = [[4.00,4.00,4.00,4.00,4.80,4.80,4.80,4.80,5.50,5.50,5.50,5.50,6.25]
-                   ,[4.00,4.00,4.00,4.00,4.80,4.80,4.80,4.80,5.50,5.50,5.50,5.50,6.25]
-                   ,[4.10,4.10,4.10,4.10,4.85,4.85,4.85,4.85,5.55,5.55,5.55,5.55,6.30]
-                   ,[4.15,4.15,4.15,4.15,4.90,4.90,4.90,4.90,5.60,5.60,5.60,5.60,6.40]
-                   ,[4.20,4.20,4.20,4.20,4.95,4.95,4.95,4.95,5.65,5.65,5.65,5.65,6.50]
-                   ,[4.25,4.25,4.25,4.25,5.00,5.00,5.00,5.00,5.70,5.70,5.70,5.70,6.55]
-                   ,[4.30,4.30,4.30,4.30,5.10,5.10,5.10,5.10,5.85,5.85,5.85,5.85,6.65]
-                   ,[4.45,4.45,4.45,4.45,5.20,5.20,5.20,5.20,5.95,5.95,5.95,5.95,6.75]
-                   ,[4.45,4.45,4.45,4.45,5.20,5.20,5.20,5.20,5.95,5.95,5.95,5.95,6.75]];          
-      cost = amount[zone][index - 1];
-      break;
-    default:
-      cost = 50;
-  }
-  return cost;
-};
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.static(path.join(__dirname, 'views/models')))
@@ -72,12 +12,10 @@ express()
   }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/postmail'))
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/week9', (req, res) => res.render('pages/postmail'))
   .post('/report', (req, res) => {
-    try {
-      //var logic = require('./models/logic.js');            
-      var amountOwed = calculateRate(req.body.type, req.body.weight, req.body.zone);
-      //var amountOwed = calculateRate("1", 1, 1);
+    try {      
       var params = {
         amount: amountOwed
       };
