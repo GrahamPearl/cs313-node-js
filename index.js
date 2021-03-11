33316b9e5c50c27caed07d10cfb9696a0f038801
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2021-03-11 04:40:33 +0200

    Upade: Fix linking method using object to handle each week task

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const Week9 = require('week9');
+const Week9 = require('./week9');
 
 var bodyParser = require("body-parser");
 
@@ -16,17 +16,16 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/week9', (req, res) => res.render('pages/postmail'))
-  .post('/week9/report', (req, res) => {
-    try {      
-      
-      Week9().handle_week9();
-      var params = {
-        amount: amountOwed
-      };
-      res.render("pages/report", params);
-    } catch (err) {
-      console.log('Error - will need to resolve:', err);
-      //res.render("pages/error-report");
-    }
-  })
+  .post('/week9/report', week9_task)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function week9_task(req, res) {
+  try {
+    const week9 = new Week9();
+          week9.handle_week9(req, res);
+  } catch (err) {
+    console.log('Error - will need to resolve:', err);
+    res.render("pages/error-report");    
+  }
+
+}
