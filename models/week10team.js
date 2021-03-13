05ab3bc8f9c186bdded4a5fc353d9d466e493efa
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2021-03-13 09:33:22 +0200

    converted from family to books_api

--- models/week10team.js
+++ models/week10team.js
@@ -4,7 +4,7 @@ const {
 
 //const uriHeroku = 'postgres://eyrisxdztiubpj:c8ca45cbe5b9cf11038936746236f52eb49c76f384467bb2e27c7e4f7bc26bf0@ec2-54-242-43-231.compute-1.amazonaws.com:5432/dbadu7beav50j0';
 //const uriHeroku = 'postgres://eyrisxdztiubpj:c8ca45cbe5b9cf11038936746236f52eb49c76f384467bb2e27c7e4f7bc26bf0@ec2-54-242-43-231.compute-1.amazonaws.com:5432/dbadu7beav50j0';
-const uriLocal = 'postgres://reg_user:family@localhost:5432/familyhistory';
+const uriLocal = 'postgres://api_user:password@localhost:5432/books_api';
 
 var URI = '';
 if (process.env._.indexOf("heroku") === -1) {
@@ -44,7 +44,7 @@ class Week10Team extends Object {
     console.log("Getting person from DB with params: " + params);
 
     //const sql = "SELECT id, first, last, birthdate FROM person WHERE last = $1 AND first = $2";
-    const sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+    const sql = "SELECT * FROM books WHERE id = $1::int";
 
     (async () => {
       const client = await pool.connect()
@@ -52,7 +52,7 @@ class Week10Team extends Object {
         const res = await client.query(sql, params)
           .then(res => {
             var params = {
-              headers: ["id", "last", "first"],
+              headers: ["id", "author", "title"],
               rows: new Array(4).fill(undefined)
             };
 
