d9afc41b4357d10cb07bd924b0fb47228aaab071
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2021-04-01 07:18:43 +0200

    Corrected route rendering

--- routes/help.js
+++ routes/help.js
@@ -1,10 +1,10 @@
 const router_library =
   require('express').Router()  
   .get('/', (req, res) => res.render('pages/library'))
-  .get('/privacy', require('pages/privacy-policy'))
-  .get('/services', require('pages/terms-of-service'))
-  .get('/library', require('pages/help-library'))
-  .get('/libraryAPI', require('pages/help-library-api'))
+  .get('/privacy', (req, res) => res.render('pages/privacy-policy'))
+  .get('/services', (req, res) => res.render('pages/terms-of-service'))
+  .get('/library', (req, res) => res.render('pages/help-library'))
+  .get('/libraryAPI', (req, res) => res.render('pages/help-library-api'))
   .get('/db', async (req, res) => {
 
   });
