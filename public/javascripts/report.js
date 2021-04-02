c0be38f3e0a5850aaa7a93cbb96f3e6d34cf487e
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2021-04-02 16:27:47 +0200

    reports updated

--- public/javascripts/report.js
+++ public/javascripts/report.js
@@ -1,3 +1,24 @@
+function btnFormatter(value, row) {
+//+value+
+  let items = ''+value+' ';
+      items += '<button type="button" class="btn btn-primary"><i class="far fa-eye"></i></button>';
+      items += '<button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>';
+      items += '<button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>';
+  return items;  
+}
+
+function add_buttons_to_build_table() {
+ 
+  $('#data-table tr').each(function () {
+    console.log("Reading Row");
+    let customButtons = $(this).find("td").eq(3).html();
+    customButtons.append('<button type="button" class="btn btn-primary"><i class="far fa-eye"></i></button>');
+    customButtons.append('<button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>');
+    customButtons.append('');
+
+  });
+}
+
 function build_table_books(info) {
   $('#data-table').bootstrapTable({
     formatLoadingMessage: function () {
@@ -9,10 +30,11 @@ function build_table_books(info) {
 
     columns: [{
       field: 'id',
-      title: 'Item ID'
+      title: 'Item ID',
+      formatter: "btnFormatter"
     }, {
       field: 'author',
-      title: 'Authors'
+      title: 'Authors',      
     }, {
       field: 'title',
       title: 'Title'
@@ -32,10 +54,11 @@ function build_table_patrons(info) {
     "pagingType": "simple",
     columns: [{
       field: 'id',
-      title: 'Item ID'
+      title: 'Item ID',
+      formatter: "btnFormatter"
     }, {
       field: 'first',
-      title: 'First Name'
+      title: 'First Name'      
     }, {
       field: 'last',
       title: 'Last Price'
@@ -45,18 +68,7 @@ function build_table_patrons(info) {
   })
 }
 
-function add_buttons_to_build_table() {
-  console.log("Reading Table");
 
-  $('#data-table tr').each(function () {
-    console.log("Reading Row");
-    let customButtons = $(this).find("td").eq(3).html();
-    customButtons.append('<button type="button" class="btn btn-primary"><i class="far fa-eye"></i></button>');
-    customButtons.append('<button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>');
-    customButtons.append('<button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>');
-
-  });
-}
 
 function find_books_all() {
   $.get("https://tranquil-river-49994.herokuapp.com/API/find_books_all",
